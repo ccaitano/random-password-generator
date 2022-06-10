2157c764ab253c125213eda3ac8cc9f843d3fbb4
ccaitano <cheryl.caitano@gmail.com>
2022-06-09 18:16:39 -1000

    Started generatePassword function and outlined steps for program

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 // Assignment code here
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var specialChar = ["~","!","@","#","$","%","^","&","*","+","?"];
-var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+function generatePassword() {
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var specialChar = ["~","!","@","#","$","%","^","&","*","+","?"];
+  var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+  //Prompt user for length of the password
+  prompt("Please choose a password length between 8 and 128 characters", "8");
+
+  //User selects password length
+
+  //Prompt user for password criteria
+
+  //User selects criteria
+
+  //Password generation
+
+  //Validate password meets criteria
+
+  //Password displayed to page
+
+
+  return;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
